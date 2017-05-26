8e77b9aa01adba07e857a057316309952adeb61b
Francisco Bretal <franciscobretal@me.com>
2017-05-26 20:11:52 +0200

    Cap Drop support

--- src/Markdown.php
+++ src/Markdown.php
@@ -5,5 +5,29 @@ namespace Flerex\LaravelMarkdown;
 use Parsedown;
 
 class Markdown extends Parsedown {
+	
+	function __construct()
+	{
+		$this->InlineTypes['^'][]= 'DropCap';
+		$this->inlineMarkerList .= '^';
+	}
+
+	public function inlineDropCap($excerpt)
+	{
+
+		if (preg_match('/^\^(.{1})/', $excerpt['text'], $matches)) {
+			return [
+				'extent'  => strlen($matches[0]),
+				'element' => [
+					'name' => 'span',
+					'text' => $matches[1],
+					'attributes' => [
+						'class' => 'drop-cap',
+					],
+				],
+			];
+		}
+		
+	}
 
 }
